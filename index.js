5d1cf95556ebbd03fe4b54487efd21a30ab6b288
abin subash <abinsubashkk@gmail,com>
2025-01-06 12:59:58 +0530

    Clock set

---
+++ index.js
@@ -0,0 +1,24 @@
+let hour = document.getElementById("Hour")
+let minute = document.getElementById("Minutes")
+let second = document.getElementById("Second")
+let time = document.getElementById("Time")
+
+ function init(){
+    let date = new Date()
+    let hourValue = date.getHours()
+    let minuteValue = date.getMinutes()
+    let secondValue = date.getSeconds()
+    if(hourValue>12){
+        time.innerHTML = "Am"
+    }else{
+        time.innerHTML = "PM"
+    }
+    hour.innerHTML = hourValue
+    minute.innerHTML = minuteValue
+    second.innerHTML = secondValue
+    requestAnimationFrame(init)
+ }
+ requestAnimationFrame(init)
+// console.log(hour)
+// console.log(minute)
+// console.log(second)
